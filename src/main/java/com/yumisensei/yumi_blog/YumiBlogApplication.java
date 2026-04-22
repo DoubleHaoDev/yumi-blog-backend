6da1445fcf225a1f084e1ad99ef11726ca4b1866
Andy Chang <yuhaoz1991@gmail.com>
2026-04-21 20:08:29 -0700

    Init project

---
+++ src/main/java/com/yumisensei/yumi_blog/YumiBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yumisensei.yumi_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumiBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumiBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yumisensei/yumi_blog/YumiBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumisensei.yumi_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumiBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
